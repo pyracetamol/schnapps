20a9d8ca00025dcae3734e359efba5870b6dc452
Delwin Perera <perera@mm.tu-darmstadt.de>
2025-05-26 09:23:21 +0200

    Adapt run script
    Regular solution model app is removed.
    
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-declare -A apps=(["regular_solution_app.py"]="8501" ["open_day_allen-cahn_app.py"]="8502" ["open_day_cahn-hilliard_app.py"]="8503" ["open_day_mc-app.py"]="8504")
+declare -A apps=(["open_day_allen-cahn_app.py"]="8501" ["open_day_cahn-hilliard_app.py"]="8502" ["open_day_mc-app.py"]="8503")
 
 for app in "${!apps[@]}"
 do
